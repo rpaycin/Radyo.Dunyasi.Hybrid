5da624454d18f6ae02437745d8fa3a0bd7b0b34f
Reşit PAYÇIN <rpaycin@gmail.com>
2015-12-15 13:56:05 +0200

    yeni web api

---
+++ Radyo.Dunyasi.WebApi/Controllers/RadioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Radyo.Dunyasi.WebApi.Controllers
+{
+    public class RadioController : ApiController
+    {
+        [HttpPost]
+        public string Test()
+        {
+            return "test";
+        }
+    }
+}
