d8a1ab657d7624f067d50c12a7c97948baea49de
Reşit PAYÇIN <rpaycin@gmail.com>
2015-12-21 21:09:50 +0200

    .

---
+++ Radyo.Dunyasi.Hybrid/www/scripts/allRadioList.js
@@ -0,0 +1,20 @@
+﻿var serviceURL = "http://212.98.202.29/StokTakip/api/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function (event) {
+    getEmployeeList();
+});
+
+function getEmployeeList() {
+    $.getJSON(serviceURL + 'radio/GetRadios', function (data) {
+        $('#employeeList li').remove();
+        employees = data.Value;
+        $.each(employees, function (index, employee) {
+            $('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.RadioId + '">' +
+					'<img src="pics/' + employee.IconUrl + '"/>' +
+					'<h4>' + employee.RadioName + '</h4>' + '</a></li>');
+        });
+        $('#employeeList').listview('refresh');
+    });
+}
