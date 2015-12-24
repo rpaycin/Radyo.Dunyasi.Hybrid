64d739483afa43f1e4cab6c1d1f898c678b64c29
Reşit PAYÇIN <rpaycin@gmail.com>
2015-12-25 01:22:27 +0200

    1

--- Radyo.Dunyasi.Hybrid/www/scripts/allRadioList.js
+++ Radyo.Dunyasi.Hybrid/www/scripts/allRadioList.js
@@ -2,19 +2,19 @@
 
 var employees;
 
-$('#employeeListPage').bind('pageinit', function (event) {
-    getEmployeeList();
+$('#pageAllRadios').bind('pageinit', function (event) {
+    getallRadioList();
 });
 
-function getEmployeeList() {
+function getallRadioList() {
     $.getJSON(serviceURL + 'radio/GetRadios', function (data) {
-        $('#employeeList li').remove();
+        $('#allRadioList li').remove();
         employees = data.Value;
         $.each(employees, function (index, employee) {
-            $('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.RadioId + '">' +
+            $('#allRadioList').append('<li><a href="employeedetails.html?id=' + employee.RadioId + '">' +
 					'<img src="pics/' + employee.IconUrl + '"/>' +
 					'<h4>' + employee.RadioName + '</h4>' + '</a></li>');
         });
-        $('#employeeList').listview('refresh');
+        $('#allRadioList').listview('refresh');
     });
 }
---
+++ Radyo.Dunyasi.Hybrid/www/scripts/mainOperations.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var menuHtml = '<ul>' +
+           '<li class="allRadios"><a href="#pageAllRadios" data-transition="slide" title="Tüm Radyolar">Tüm Radyolar</a></li>' +
+           '<li class="categories"><a href="#pageCategories" data-transition="slide" title="Kategoriler">Kategoriler</a></li>' +
+           '<li class="favourites"><a href="#" data-transition="slide" title="Favorilerim">Favorilerim</a></li>' +
+       '</ul>';
+
+    $("#panelAllRadios").append(menuHtml);
+    $("#panelCategories").append(menuHtml);
+});
