7c8885433d5fba026c4df9a0b82eb83be816dca2
Reşit PAYÇIN <rpaycin@gmail.com>
2015-12-25 19:43:37 +0200

    1

---
+++ Radyo.Dunyasi.Hybrid/www/scripts/common.js
@@ -0,0 +1,17 @@
+﻿function isAndroid() {
+    var nua = navigator.userAgent;
+
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android') > -1 && nua.indexOf('AppleWebKit') > -1);
+    return isAndroid;
+    }
+
+function isIpad() {
+    var nua = navigator.userAgent;
+
+    var isiPad = nua.match(/iPad/i) != null;
+    return isiPad;
+}
+
+function isIOS() {
+    return navigator.userAgent.match(/(iPad|iPhone|iPod)/g);
+}
--- Radyo.Dunyasi.Hybrid/www/scripts/mainOperations.js
+++ Radyo.Dunyasi.Hybrid/www/scripts/mainOperations.js
@@ -1,10 +1,27 @@
-﻿$(function () {
+﻿//ana menuyu sayfalara ekleme
+mainMenuAdd();
+
+//statusbari aşağı kaydırma ios için
+addHeaderMarginTop();
+
+function mainMenuAdd() {
     var menuHtml = '<ul>' +
-           '<li class="allRadios"><a href="#pageAllRadios" data-transition="slide" title="Tüm Radyolar">Tüm Radyolar</a></li>' +
-           '<li class="categories"><a href="#pageCategories" data-transition="slide" title="Kategoriler">Kategoriler</a></li>' +
-           '<li class="favourites"><a href="#" data-transition="slide" title="Favorilerim">Favorilerim</a></li>' +
-       '</ul>';
+       '<li class="allRadios"><a href="#pageAllRadios" data-transition="slide" title="Tüm Radyolar">Tüm Radyolar</a></li>' +
+       '<li class="categories"><a href="#pageCategories" data-transition="slide" title="Kategoriler">Kategoriler</a></li>' +
+       '<li class="favourites"><a href="#" data-transition="slide" title="Favorilerim">Favorilerim</a></li>' +
+   '</ul>';
 
     $("#panelAllRadios").append(menuHtml);
     $("#panelCategories").append(menuHtml);
-});
+}
+
+function addHeaderMarginTop() {
+    var isios = isIOS();
+    if (isios != null && isios) {
+        var px = "20px";
+        $("#headerAllRadios").css("margin-top", px);
+        $("#panelAllRadios").css("margin-top", px);
+        $("#headerCategories").css("margin-top", px);
+        $("#panelCategories").css("margin-top", px);
+    }
+}
