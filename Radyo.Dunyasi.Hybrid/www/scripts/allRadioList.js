330de340ab91dfaf9cc917a657327e2cbfcd27f8
Reşit PAYÇIN <rpaycin@gmail.com>
2015-12-28 21:47:02 +0200

    1

--- Radyo.Dunyasi.Hybrid/www/scripts/allRadioList.js
+++ Radyo.Dunyasi.Hybrid/www/scripts/allRadioList.js
@@ -17,13 +17,7 @@ function getallRadioList() {
 
             $.each(data.Value, function (index, radio) {
                 if (radio.IsShow) {
-
-                    var imageUrl = radio.IsImageLocal ? localImageURL : serverImageURL;
-                    imageUrl = imageUrl + radio.IconUrl;
-
-                    $('#allRadioList').append('<li style="height:50px;" ><a href="employeedetails.html?id=' + radio.RadioId + '">' +
-                    '<img style="margin-left:10px;" src=' + imageUrl + '></img>' +
-                    '<h4>' + radio.RadioName + '</h4>' + '</a></li>');
+                    $('#allRadioList').append(getRadioItem(radio));
                 }
             });
 
@@ -32,4 +26,25 @@ function getallRadioList() {
         else
             alert('radyo bilgileri alınamadı!');
     });
+}
+
+function getRadioItem(radio) {
+    var imageUrl = radio.IsImageLocal ? localImageURL : serverImageURL;
+    imageUrl = imageUrl + radio.IconUrl;
+
+    var radioItem="<li data-icon='false'> \
+                    <a href='#'> \
+                        <img src='" + imageUrl + "' class='circleImage'> \
+                        <h style='margin-left:-20px;'>" + radio.RadioName + "</h> \
+                        <table style='margin-left:-20px;'> \
+                            <tr>\
+                                <td><p style='background-color:#FFEBCD;font-size:10px'>POP</p></td> \
+                                <td><p style='background-color:#ADD8E6;font-size:10px'>POP</p></td>\
+                                <td><p style='background-color:#98FB98;font-size:11px'>Türk Sanat Müziği</p></td>\
+                            </tr>\
+                        </table>\
+                    </a>\
+                </li>";
+
+    return radioItem;
 }
