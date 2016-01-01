ab25051e6ef671d8c0556134eb66e4ce0ed5d480
Reşit PAYÇIN <rpaycin@gmail.com>
2016-01-01 20:17:31 +0200

    1

--- Radyo.Dunyasi.Hybrid/www/scripts/pageAllRadios.js
+++ Radyo.Dunyasi.Hybrid/www/scripts/pageAllRadios.js
@@ -17,7 +17,7 @@ var storeObject = {
 }
 
 $(document).on('pagebeforeshow', '#pageAllRadios', function () {
-    $(document).on('click', '#testbutton', function () {
+    $(document).on('click', '.radyoItem', function () {
         // store some data
         storeObject.firstname = 'Dragan';
         storeObject.lastname = 'Gaic';
