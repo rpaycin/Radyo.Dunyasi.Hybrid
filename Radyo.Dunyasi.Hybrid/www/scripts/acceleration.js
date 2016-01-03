ff23b3bbfe3484aef44224229a1619a13d5f05be
Reşit PAYÇIN <rpaycin@gmail.com>
2016-01-03 10:36:09 +0200

    1

---
+++ Radyo.Dunyasi.Hybrid/www/scripts/acceleration.js
@@ -0,0 +1,33 @@
+﻿//o anki sallama durumunu al
+function getAcceleration() {
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
+
+var watchID;
+
+//sallamayı belli n saniye aralıklarla düzenli izle
+function startWatchAccelerator() {
+    var options = { frequency: 1000 };
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+
+//sallama izlemeyi durdur
+function stopWatchAccelerator() {
+    if (watchID) {
+        navigator.accelerometer.clearWatch(watchID);
+        document.getElementById("divAcceleration").innerHTML = "İzleme durduruldu";
+        watchID = null;
+    }
+}    // ACCELERATOR EVENTLERİ
+//cihaz sallandığı anda çalışır
+function onSuccess(acceleration) {
+    document.getElementById("divAcceleration").innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: ' + acceleration.timestamp + '<br />';
+}
+
+//cihaz sallandığı zaman bir hata oluşursa çalışır
+function onError() {
+    alert('onError!');
+}
