f535bf6673a269294e858edd826242f7e416e17d
Reşit PAYÇIN <rpaycin@gmail.com>
2016-01-31 23:56:50 +0200

    1

--- Radyo.Dunyasi.WebMobile/scripts/mainOperations.js
+++ Radyo.Dunyasi.WebMobile/scripts/mainOperations.js
@@ -52,7 +52,7 @@ function mainMenuAdd() {
     var menuHtml = '<ul>' +
        '<li class="allRadios"><a href="#pageAllRadios" data-transition="slide" title="Tüm Radyolar">Tüm Radyolar</a></li>' +
        '<li class="categories"><a href="#pageCategories" data-transition="slide" title="Kategoriler">Kategoriler</a></li>' +
-       '<li class="favourites"><a href="#" data-transition="slide" title="Favorilerim">Favorilerim</a></li>' +
+       //'<li class="favourites"><a href="#" data-transition="slide" title="Favorilerim">Favorilerim</a></li>' +
    '</ul>';
 
     $("#panelAllRadios").append(menuHtml);
