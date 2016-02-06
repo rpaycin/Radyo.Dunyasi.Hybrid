4413a8ea08e5dd9f97fbfdc6807ecb35e49833ca
Reşit PAYÇIN <rpaycin@gmail.com>
2016-02-06 18:16:17 +0200

    1

--- Radyo.Dunyasi.WebMobile/scripts/modelsAndVariables.js
+++ Radyo.Dunyasi.WebMobile/scripts/modelsAndVariables.js
@@ -3,7 +3,7 @@ var serverUrl = "http://212.98.202.29/StokTakip/";
 var serviceURL = serverUrl + "api/";
 
 var serverImageURL = serverUrl + "Contents/Images/Radyolar/";
-var localImageURL = "images/radyolar/";
+var localImageURL = "img/radyolar/";
 var isWorkAllRadio = true;
 var isAllCategory = true;
 
